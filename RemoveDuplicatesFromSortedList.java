3d0a115a574aabafd472ecd90c0ba0d042902f76
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-04-02 00:19:40 +0300

    Create RemoveDuplicatesFromSortedList.java

---
+++ RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,19 @@
+ public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode prev = dummy;
+        while(cur != null){
+            if(cur.next != null && cur.val == cur.next.val){
+                while(cur.next != null && cur.val == cur.next.val){
+                    cur = cur.next;
+                }
+                prev.next = cur.next;
+            }else{
+                prev = prev.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
